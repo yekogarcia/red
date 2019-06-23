7a8fa9f5d9d9fe4d9713007d132a49c7ba06512d
Yeko Garcia <yekogarcia@yahoo.com>
2019-06-22 21:21:01 -0500

    jdjdjd

--- js/login.js
+++ js/login.js
@@ -1,15 +1,17 @@
-$(document).ready(function(e) {
-  $('#recover').click(function(e) {
-    e.preventDefault();
-    
-    $('.splas').hide();
-    $('.recuperar').show();
-  });
-  
-  $("#registro").click(function(e){
-    e.preventDefault();
+$(document).ready(function (e) {
+  $('#recover').click(function (e) {
+    e.preventDefault()
 
-    $('.splas').hide();
-    $(".registro_user").show();
+    $('.splas').hide()
+    $('.registro_user').hide()
+    $('.recuperar').show()
   })
-});
+
+  $('#registro').click(function (e) {
+    e.preventDefault()
+
+    $('.recuperar').hide()
+    $('.splas').hide()
+    $('.registro_user').show()
+  })
+})
